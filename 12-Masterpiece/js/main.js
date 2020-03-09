fe47992fe686a85e92f911f6062e926d3a52c411
Romain <47246954+RomainSire@users.noreply.github.com>
2020-03-09 16:44:12 +0100

    Add files via upload

--- 12-Masterpiece/js/main.js
+++ 12-Masterpiece/js/main.js
@@ -1,1 +1,28 @@
-test
+'use strict';
+
+/* ----------------------------------------------------------- *|
+  CODE PRINCIPAL (exécuté lorsque l'arbre html est chargé)
+|* ----------------------------------------------------------- */
+$(function() {
+
+  // Gestionnaires d'évennements
+  // Evenement click sur le canvas #masterpiece
+  $('#masterpiece').on('click', onClickOnCanvas);
+  // Evenement sur le click des boutons
+  $('button').on('click', onClickOnButton);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
